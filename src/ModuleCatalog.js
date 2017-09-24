24d4ddab171bc12c111f23993ba47f830304ed51
Felipe Sere <felipesere@gmail.com>
2017-09-24 22:10:18 +0100

    Extract modules into array

--- src/ModuleCatalog.js
+++ src/ModuleCatalog.js
@@ -4,18 +4,40 @@ import LearningModule from './LearningModule.js'
 class ModuleCatalog extends React.Component {
 
   render() {
+
+    let modules = [
+      {
+        title: "Java 3x3 TTT",
+        subtitle: "Learn the basics of a Java CLI app"
+      },
+      {
+        title: "Java 4x4 performance",
+        subtitle: "Learn the basics of a Java CLI app"
+      },
+      {
+        title: "Java Echo Server",
+        subtitle: "What are Sockets and why do they matter?"
+      },
+      {
+        title: "Java Chat server",
+        subtitle: "Talking is fun!"
+      },
+      {
+        title: "Java HTTP server",
+        subtitle: "A challenge to be experienced!"
+      },
+    ]
+
+    let learningModules = modules.map( (module, idx) => {
+      return (
+        <LearningModule key={idx} title={module.title} subtitle={module.subtitle} ></LearningModule>
+      );
+    });
+
+    
     return (
-      <div>
-        <LearningModule
-            title="Java 3x3 TTT"
-            subtitle="Learn the basics of a Java CLI app"
-        >
-        </LearningModule>
-        <LearningModule
-            title="Java 4x4 with Performance TTT"
-            subtitle="Learn to measure performance and track bottlenecks"
-        >
-        </LearningModule>
+      <div className="module-catalog">
+      {learningModules}
       </div>
     );
   }
