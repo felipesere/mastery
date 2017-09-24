c1079d91c724426bd7d8108373c3ce0113c4f7c7
Felipe Sere <felipesere@gmail.com>
2017-09-24 18:19:06 +0100

    Minimal Hello React added

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path')
+
+const config = {
+  entry: './src/app.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2016', 'react']
+        }
+      }
+    ]
+  }
+}
+
+module.exports = config
