5172899d135e85ea81439870f749d969d32fdcbb
Felipe Sere <felipesere@gmail.com>
2017-09-24 23:28:14 +0100

    Adds a modal!

--- src/LearningModule.js
+++ src/LearningModule.js
@@ -2,6 +2,24 @@ import React from 'react';
 
 class LearningModule extends React.Component {
 
+  constructor(props) {
+    super(props)
+    this.openModal = this.openModal.bind(this)
+    this.closeModal = this.closeModal.bind(this)
+    this.state = {detailsOpen: false}
+
+  }
+
+  openModal(e) {
+    e.preventDefault()
+    this.setState({detailsOpen: true})
+  }
+
+  closeModal(e) {
+    e.preventDefault()
+    this.setState({detailsOpen: false})
+  }
+
   render() {
     let style = {
       margin: '1em',
@@ -21,8 +39,25 @@ class LearningModule extends React.Component {
           </div>
         </div>
         <footer className="card-footer">
-          <a href="#" className="button card-footer-item">Open</a>
+          <a onClick={this.openModal} href="#" className="button card-footer-item">Open</a>
         </footer>
+
+        <div className={"modal " + (this.state.detailsOpen ? 'is-active' : '')}>
+          <div className="modal-background"></div>
+            <div className="modal-card">
+            <header className="modal-card-head">
+               <p className="modal-card-title">{this.props.title}</p>
+               <button onClick={this.closeModal} className="delete" aria-label="close"></button>
+            </header>
+            <section className="modal-card-body">
+                Bla bla bla
+            </section>
+            <footer className="modal-card-foot">
+              <button onClick={this.closeModal} className="button is-success">Save changes</button>
+              <button onClick={this.closeModal} className="button">Cancel</button>
+            </footer>
+          </div>
+        </div>
       </div>
     );
   }
