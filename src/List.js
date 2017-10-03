bde903d9efb64a46178a8dd0c012b4cee031c68c
Felipe Sere <felipesere@gmail.com>
2017-10-03 17:47:53 +0100

    Exmpty lists just retunrn an empty div, rather than blowing up

--- src/List.js
+++ src/List.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default class List extends React.Component {
   render() {
     let elements = this.props.elements;
-    if(elements.length === 0) { return }
+    if(elements.length === 0) { return <div></div> }
 
     let listItems = elements.map( (element, idx) => {
       return <li key={idx}>{element}</li>
