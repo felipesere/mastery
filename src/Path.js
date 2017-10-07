18503bcf98c521eeb81c1eb6d45be69520adc098
Felipe Sere <felipesere@gmail.com>
2017-10-07 14:43:50 +0100

    Move the styling of sidebar into CSS

--- src/Path.js
+++ src/Path.js
@@ -12,14 +12,6 @@ export default class Path extends React.Component {
       return (null)
     }
 
-    let style = {
-      float: "left",
-      width: "300px",
-      height: "100%",
-      background: "white",
-      boxShadow: "0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24), 0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22)",
-    }
-
     // TODO improve
     let learningModules = this.props.modules.map( (module, idx) => {
       return (
@@ -29,7 +21,7 @@ export default class Path extends React.Component {
 
 
     return (
-      <div style={style}>
+      <div className="path">
         {learningModules}
       </div>
     )
