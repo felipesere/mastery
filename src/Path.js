d4fb1fe287276698b96cd2bcc17cf4a4aded5e05
Felipe Sere <felipesere@gmail.com>
2017-10-07 14:35:06 +0100

    Don't show the sidebar if there are no selected modules

--- src/Path.js
+++ src/Path.js
@@ -8,13 +8,16 @@ export default class Path extends React.Component {
   }
 
   render() {
+    if(this.props.modules.length == 0) {
+      return (null)
+    }
+
     let style = {
       float: "left",
       width: "300px",
       height: "100%",
       background: "white",
-      boxShadow: "0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24), 0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22)"
-
+      boxShadow: "0 1px 3px rgba(0,0,0,0.12), 0 1px 2px rgba(0,0,0,0.24), 0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22)",
     }
 
     // TODO improve
