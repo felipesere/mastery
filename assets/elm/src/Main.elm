5ea4ca6392b6379557297b2f530f27786e877efe
Felipe Sere <felipesere@gmail.com>
2017-10-15 20:21:18 +0100

    Yay. Fix the path column not being full height

--- assets/elm/src/Main.elm
+++ assets/elm/src/Main.elm
@@ -5,6 +5,7 @@ import Backend
 import DetailsModal
 import Header
 import Html
+import Html.Attributes exposing (class)
 import Messages exposing (..)
 import ModuleCatalog
 import Path
@@ -52,10 +53,17 @@ view : Model -> Html.Html Msg
 view model =
     Html.div []
         [ Header.view
-        , Path.view model.selectedLessons
-        , ModuleCatalog.view (Search.forTerm model.search model.lessons)
+        , columns
+            [ Path.view model.selectedLessons
+            , ModuleCatalog.view (Search.forTerm model.search model.lessons)
+            ]
         , DetailsModal.view model.modal
         ]
+
+
+columns : List (Html.Html Msg) -> Html.Html Msg
+columns elements =
+    Html.div [ class "columns" ] elements
 
 
 main =
