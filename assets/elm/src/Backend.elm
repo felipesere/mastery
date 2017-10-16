dded4fc63f5426ceed9c518c3a37ecb0184bcf3e
Felipe Sere <felipesere@gmail.com>
2017-10-16 08:37:35 +0100

    De-anonymize function that defaults errors to empty lists of lessons

--- assets/elm/src/Backend.elm
+++ assets/elm/src/Backend.elm
@@ -7,16 +7,16 @@ import Messages exposing (Msg(..))
 
 get : String -> Cmd Msg
 get url =
-    let
-        default =
-            \result ->
-                result
-                    |> Result.withDefault []
-                    |> LoadModules
-    in
     Http.send default <| loadLessons url
 
 
 loadLessons : String -> Http.Request (List Lesson.Lesson)
 loadLessons url =
     Http.get (url ++ "/api/lessons") Lesson.decodeList
+
+
+default : Result Http.Error (List Lesson.Lesson) -> Msg
+default result =
+    result
+        |> Result.withDefault []
+        |> LoadModules
