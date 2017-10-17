6038f3224d31fb7063f65011a52d219ba78031a3
Felipe Sere <felipesere@gmail.com>
2017-10-17 19:58:53 +0100

    Prepare for Heroku

--- config/prod.exs
+++ config/prod.exs
@@ -15,10 +15,19 @@ use Mix.Config
 # which you typically run after static files are built.
 config :mastery_backend, MasteryBackendWeb.Endpoint,
   load_from_system_env: true,
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  url: [scheme: "https", host: "path2mastery.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
-config :mastery_backend, :lessons_file, "data/lessons.json"
+config :mastery_backend, :lessons_file, "sampleLessons.json"
+#
+# Configure your database
+config :mastery_backend, MasteryBackend.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
 
 # Do not print debug messages in production
 config :logger, level: :info
