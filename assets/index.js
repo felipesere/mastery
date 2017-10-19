75e3767926a802357f95a847eb7d848ba7b869ec
Felipe Sere <felipesere@gmail.com>
2017-10-19 17:37:39 +0100

    Generete all the files into the right places

---
+++ assets/index.js
@@ -0,0 +1,16 @@
+'use strict';
+
+require('./static/index.html');
+var Elm = require('./elm/src/Main.elm');
+
+
+var sha = COMMIT_HASH
+var time = BUILD_TIME
+var baseUrl = MODULES_URL
+
+
+var app = Elm.Main.fullscreen({
+  baseUrl: baseUrl,
+  buildTime: time,
+  commit: sha,
+});
--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -1,11 +1,16 @@
-var webpack = require('webpack')
+const webpack = require('webpack')
+const GitRevisionPlugin = require('git-revision-webpack-plugin')
+const moment = require('moment')
+const git = new GitRevisionPlugin()
+
 
 module.exports = {
   entry:  {
-    app: __dirname + '/elm/src/Main.elm',
+    'js/app.js': __dirname + '/index.js'
   },
   output: {
-    filename: 'app.js'
+    path: __dirname + '/../priv/static/',
+    filename: '[name]'
   },
 
   resolve: {
@@ -14,8 +19,24 @@ module.exports = {
 
   module: {
     loaders: [
-      { test: /\.elm$/, loader: 'elm-webpack-loader?cwd='+__dirname+'/elm&verbose=true&warn=true', exclude: [/elm-stuff/, /node-modules/]},
+      { 
+        test: /\.elm$/,
+        loader: 'elm-webpack-loader?cwd='+__dirname+'/elm&verbose=true&warn=true',
+        exclude: [/elm-stuff/, /node-modules/]
+      },
+      {
+        test:    /\.html$/,
+        exclude: /node_modules/,
+        loader:  'file-loader?name=[name].[ext]',
+      },
     ],
     noParse: /\.elm$/
-  }
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'MODULES_URL': JSON.stringify(process.env.MODULES_URL || 'http://localhost:4000/'),
+      'COMMIT_HASH': JSON.stringify(process.env.SOURCE_VERSION || JSON.stringify(git.commithash())),
+      'BUILD_TIME': JSON.stringify(moment().format('LLLL') || "No build time derived")
+    })
+  ]
 }
