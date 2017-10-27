bc72b145df34b92981cc3f9108035dbdb2301eca
Felipe Sere <felipesere@gmail.com>
2017-10-27 12:45:28 +0100

    Nicer styling the personal path

---
+++ assets/elm/src/SmallCard.elm
@@ -0,0 +1,49 @@
+module SmallCard exposing (..)
+
+import Html exposing (..)
+import Html.Attributes exposing (class, style)
+import Language exposing (Language)
+import Lesson exposing (Lesson)
+import Style exposing (..)
+
+
+type State
+    = Done
+    | Todo
+
+
+view : State -> Lesson -> Html a
+view state lesson =
+    case state of
+        Done ->
+            done lesson
+
+        Todo ->
+            todo lesson
+
+
+done lesson =
+    Html.div [ class "small-card", style (border lesson.language) ]
+        [ Html.div [ class "small-card-title" ] [ Html.text lesson.title, checkmark ]
+        ]
+
+
+todo lesson =
+    Html.div [ class "small-card", style (border lesson.language) ]
+        [ title lesson
+        ]
+
+
+checkmark =
+    span [ class "icon", style [ Style.color "green" ] ]
+        [ i [ class "fa fa-check fa-fw" ] [] ]
+
+
+title lesson =
+    Html.div [ class "small-card-title" ] [ Html.text lesson.title ]
+
+
+border : Language -> List Style
+border language =
+    [ Style.border <| "1px solid " ++ Language.toColor language
+    ]
