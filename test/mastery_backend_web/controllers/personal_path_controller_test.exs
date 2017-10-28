bb9367533626497f4d996adaf5b0ca4ff2f33b82
Felipe Sere <felipesere@gmail.com>
2017-10-28 18:47:15 +0100

    Save paths to an agent

---
+++ test/mastery_backend_web/controllers/personal_path_controller_test.exs
@@ -0,0 +1,28 @@
+defmodule MasteryBackendWeb.PersonalPathControllerTest do
+  use MasteryBackendWeb.ConnCase
+
+  setup do
+    user =  %MasteryBackend.Github.User{name: "Ron Swanson", id: "12", github_token: nil}
+    MasteryBackend.Users.save(user)
+
+    [user: user]
+  end
+
+  test "can create a new path", %{conn: conn, user: user} do
+    data = %{:modules => [1,2,3,4,5]}
+
+    conn
+    |> authenticated_as(user.id)
+    |> post("/api/path", data)
+    |> response(200)
+
+    assert conn
+           |> authenticated_as(user.id)
+           |> get("/api/path")
+           |> json_response( 200) === %{"todo" => [1,2,3,4,5], "current" => "none", "done" => []}
+  end
+
+  defp authenticated_as(conn, id) do
+    put_req_cookie(conn, "auth", MasteryBackend.Secure.encrypt(id))
+  end
+end
