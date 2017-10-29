31f37d3aa7bc979770c6ab640a8ff7b78462c531
Felipe Sere <felipesere@gmail.com>
2017-10-29 20:07:45 +0000

    [elm] Use result from saving selected lessons to create path

---
+++ assets/elm/tests/PathDecoderTests.elm
@@ -0,0 +1,48 @@
+module PathDecoderTests exposing (..)
+
+import Expect exposing (Expectation)
+import Json.Decode as Decode
+import Language exposing (Language)
+import Lesson
+import PersonalPath
+import Test exposing (..)
+
+
+suite : Test
+suite =
+    describe "How to parse the lessons JSON"
+        [ test "parses with reading list" <|
+            \_ ->
+                let
+                    json =
+                        """
+                        {
+                          "done": [],
+                          "current": null,
+                          "todo": []
+                        }
+                        """
+
+                    output =
+                        Decode.decodeString PersonalPath.decode json
+                in
+                Expect.equal output
+                    (Ok
+                        { completed = []
+                        , current = Nothing
+                        , todo = []
+                        }
+                    )
+        ]
+
+
+lesson =
+    { id = Lesson.Id 1
+    , title = "Elixir Guilded Rose"
+    , subtitle = "Refactoring and pattern matching"
+    , description = "Bla bla bla"
+    , language = Language.fromString "Elixir"
+    , outputs = []
+    , outcomes = []
+    , readingMaterial = []
+    }
